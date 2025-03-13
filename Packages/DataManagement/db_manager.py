24ae470acc5fdd13f084b6cf56f7123ae45d7ec3
ItsPetya <140097543+JustPetya@users.noreply.github.com>
2025-03-13 14:20:53 +0100

    Create db_manager.py
    database

---
+++ Packages/DataManagement/db_manager.py
@@ -0,0 +1,20 @@
+import sqlite3 as sql
+
+
+
+class Mod:
+    def __init__(self):
+        pass
+
+    def db_generate(self):
+        pass
+
+    def create_tables(self):
+        pass
+
+    def table_generator(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
